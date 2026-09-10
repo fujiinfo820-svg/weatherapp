febf6c0544794eda06c82de7388591bb3a611606
kosuke <fuji.info820@gmail.com>
2026-09-10 22:21:43 +0900

    Add authentication feature using Laravel Breeze with CDN setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherController;
 
-// トップページにアクセスしたら WeatherController の index メソッドを実行
-Route::get('/', [WeatherController::class, 'index'])->name('weather.index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
